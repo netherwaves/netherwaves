3635e44ca65805a7aab3d8a5a27b1e0f55082929
Julien Vermette <julien.vermette@outlook.com>
2022-01-08 12:17:11 -0500

    add graphql and fix schema script

--- frontend/pages/works/index.vue
+++ frontend/pages/works/index.vue
@@ -1,22 +1,48 @@
 <template>
     <section class="container">
-        <diag-line></diag-line>
         <p>works</p>
         <nuxt-link to="works/single">go to single</nuxt-link>
+
+        <article v-for="(article, i) in articles" :key="i">
+            <h2>{{ article.title }}</h2>
+        </article>
     </section>
 </template>
 
 <script>
     import DiagLine from '~/components/svg/DiagLine.vue';
 
+    import meta from '~/plugins/page-meta.mixin.js';
+    import worksQuery from '~/queries/works.gql';
+    import gql from 'graphql-tag';
+
     export default {
+        mixins: [meta],
+
         components: {
             'diag-line': DiagLine,
+        },
+
+        computed: {
+            articles() {
+                return this.entries.edges.map(edge => edge.node);
+            }
         },
 
         head() {
             return {
                 title: 'works | netherwaves'
+            }
+        },
+
+        apollo: {
+            entries: {
+                query: worksQuery,
+                variables() {
+                    return {
+                        limit: 5
+                    }
+                }
             }
         }
     }
@@ -25,5 +51,7 @@
 <style lang="scss">
 .container {
     position: relative;
+    display: flex;
+    align-items: center;
 }
 </style>
