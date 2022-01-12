648b7d001ddc4b380501884b747e945d662ae487
Julien Vermette <julien.vermette@outlook.com>
2022-01-11 22:47:11 -0500

    add favicons

--- hubworld/nuxt.config.js
+++ hubworld/nuxt.config.js
@@ -14,11 +14,18 @@ module.exports = {
     head: {
         title: pkg.name,
         meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'msapplication-TileColor', content: '#da532c' },
+            { name: 'theme-color', content: '#181818' }
         ],
         link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
         ]
     },
 
