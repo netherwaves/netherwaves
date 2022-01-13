9193b0518817f8c4b58198d421c7c664a1008340
Julien Vermette <julien.vermette@outlook.com>
2022-01-13 13:55:10 -0500

    change favicon config

--- hubworld/nuxt.config.js
+++ hubworld/nuxt.config.js
@@ -16,8 +16,7 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'msapplication-TileColor', content: '#da532c' },
-            { name: 'theme-color', content: '#181818' }
+            { name: 'msapplication-TileColor', content: '#b91d47' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -25,7 +24,7 @@ module.exports = {
             { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
             { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
             { rel: 'manifest', href: '/site.webmanifest' },
-            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+            { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#181818 ' },
         ]
     },
 
